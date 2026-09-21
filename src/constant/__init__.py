01ec0e646737cb1b656a86bcadde75e96d6a8357
Bhagesh Sharma <bhageshsharma777@gmail.com>
2026-09-21 19:12:06 +0530

    modi

---
+++ src/constant/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+AWS_S3_BUCKET_NAME = "waferfault"
+MONGO_DATABASE_NAME = "pwskils"
+MONGO_COLLECTION_NAME = "wafer_fault"
+
+TARGET_COLUMN = "quality"
+MONGO_DB_URL = mongodb+srv://bhageshsharma777_db_user:oL05ETjp0HTDe6Bv@cluster0.t2vd1tm.mongodb.net/?appName=Cluster0
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
